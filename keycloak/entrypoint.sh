6317d54a920fd3edcefcf0b20de3178d5471ce67
Muhend <hamohand@gmail.com>
2025-11-05 16:21:33 +0100

    Modif entrypoint.sh: dossier tmp

--- keycloak/entrypoint.sh
+++ keycloak/entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 # Créer le répertoire s'il n'existe pas
 mkdir -p /opt/keycloak/data/import
 
-# Remplacer les variables d'environnement dans realm-export.json
-envsubst < /opt/keycloak/data/import/realm-export.json.template > /opt/keycloak/data/import/realm-export.json
+# Remplacer les variables d'environnement dans realm-export.json (écrire dans /tmp d'abord)
+TMP_FILE="/tmp/realm-export.json"
+envsubst < /opt/keycloak/data/import/realm-export.json.template > "$TMP_FILE"
 
-# S'assurer que le fichier généré a les bonnes permissions
+# Copier le fichier dans le répertoire d'import avec les bonnes permissions
+cp "$TMP_FILE" /opt/keycloak/data/import/realm-export.json
 chmod 644 /opt/keycloak/data/import/realm-export.json
+rm "$TMP_FILE"
 
 # Exécuter la commande Keycloak originale
 exec /opt/keycloak/bin/kc.sh "$@"
